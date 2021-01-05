60a9cec87f3d2ee45a91fd0a83f2bb1815fe7fda
Alexander Mahiboroda <7674325-KaneMaster89@users.noreply.gitlab.com>
2021-01-05 23:11:52 +0300

    Добавьте файлы проекта.

---
+++ BudgetCalculation/BudgetElement.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BudgetCalculation
+{
+    class BudgetElement : Object
+    {
+        private double price;
+        private int countGoods;
+        private double summ;
+
+        public string Group { set; get; }
+        public string SubGroup { set; get; }
+        public string Name { set; get; }        
+        public double Price {private set => price = (value < 0) ? 0 : value; get => price;}
+        public bool IsExpense { set; get; }
+        public int CountGoods { get; }
+        public double Summ { get; }
+
+        public BudgetElement( string group, string subGroup, string Name, double price, bool isExpense, int countGoods)
+        {
+
+        }
+
+
+    }
+}
---
+++ BudgetCalculation/BudgetList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BudgetCalculation
+{
+    class BudgetList
+    {
+        
+        List<BudgetElement> elements = new List<BudgetElement>();
+
+        summary c
+
+    }
+}
